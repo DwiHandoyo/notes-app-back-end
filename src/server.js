acdf22a27c2f17e76aad60af9922b88ed942840b
DwiHandoyo <dhandoyo14@gmail.com>
2021-07-10 18:01:58 +0700

    export notes implemented

--- src/server.js
+++ src/server.js
@@ -22,6 +22,11 @@ const collaborations = require('./api/collaborations');
 const CollaborationsService = require('./services/postgres/CollaborationsService');
 const CollaborationsValidator = require('./validator/collaborations');
 
+// Exports
+const _exports = require('./api/exports');
+const ProducerService = require('./services/rabbitmq/ProducerService');
+const ExportsValidator = require('./validator/exports');
+
 require('dotenv').config();const init = async () => {
   const collaborationsService = new CollaborationsService();
   const notesService = new NotesService(collaborationsService);
@@ -89,6 +94,13 @@ require('dotenv').config();const init = async () => {
         collaborationsService,
         notesService,
         validator: CollaborationsValidator,
+      },
+    },
+    {
+      plugin: _exports,
+      options: {
+        service: ProducerService,
+        validator: ExportsValidator,
       },
     },
   ]);
